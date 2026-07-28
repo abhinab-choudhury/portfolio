5307ae16494cb5e79568150d16e058c3d866a273
Abhinab Choudhury <132006996+abhinab-choudhury@users.noreply.github.com>
2026-07-28 22:11:35 +0530

    Update navbar header width to full

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
   return (
     <div className="flex justify-between align-bottom p-6 md:p-10 w-full border-b-2 border-dashed border-foreground mb-2">
       <div className="flex flex-col items-start md:flex-row gap-2">
-        <h1 className="font-bold tracking-tight text-2xl md:text-3xl w-[90%] flex gap-3 items-center">
+        <h1 className="font-bold tracking-tight text-2xl md:text-3xl w-full flex gap-3 items-center">
           <Octagon className="fill-accent" />
           
           <div 
