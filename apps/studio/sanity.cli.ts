a0b57d4a744bf37af0ac5a5b88c37c519e2b0eea
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2026-08-22 12:30:19 +0530

    added .env for the sanity-studio project

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -2,8 +2,8 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '2iywv1so',
-    dataset: 'production',
+    projectId: process.env.SANITY_PROJECT_ID!,
+    dataset: process.env.SANITY_DATASET!,
   },
   deployment: {
     autoUpdates: true,
--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -1,14 +1,14 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
   title: 'portfolio',
 
-  projectId: '2iywv1so',
-  dataset: 'production',
+  projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: import.meta.env.SANITY_STUDIO_DATASET,
 
   plugins: [structureTool(), visionTool()],
 
